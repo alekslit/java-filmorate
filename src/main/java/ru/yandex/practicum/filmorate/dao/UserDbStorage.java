c15b84d9d729b36e06bd8759745d178ca3b4602a
elizaveta-mokritskaya <aro.volturi0411@yandex.ru>
2024-01-03 18:32:07 +0300

    общие фильмы по лайкам codestyle 2

--- src/main/java/ru/yandex/practicum/filmorate/dao/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/UserDbStorage.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.exception.AlreadyExistException;
 import ru.yandex.practicum.filmorate.exception.IllegalIdException;
 import ru.yandex.practicum.filmorate.exception.InvalidDataBaseQueryException;
-import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.sql.ResultSet;
