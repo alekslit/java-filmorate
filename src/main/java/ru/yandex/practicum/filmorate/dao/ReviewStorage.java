50a5d872d665ab2d45e9aee5cb99a08640f9d048
elizaveta-mokritskaya <aro.volturi0411@yandex.ru>
2024-01-10 22:00:49 +0300

    add-reviews

---
+++ src/main/java/ru/yandex/practicum/filmorate/dao/ReviewStorage.java
@@ -0,0 +1,24 @@
+package ru.yandex.practicum.filmorate.dao;
+
+import ru.yandex.practicum.filmorate.model.Review;
+import java.util.List;
+
+public interface ReviewStorage {
+    Review findReviewById(Long id);
+
+    Review addReview(Review review);
+
+    Review updateReviewById(Review review);
+
+    String deleteReviewById(Long id);
+
+    List<Review> findAllReviews(Long filmId, Integer count);
+
+    String likeReview(Long reviewId, Long userId);
+
+    String dislikeReview(Long reviewId, Long userId);
+
+    String removeLikeFromReview(Long reviewId, Long userId);
+
+    String removeDislikeFromReview(Long reviewId, Long userId);
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/service/ReviewDbService.java
@@ -0,0 +1,63 @@
+package ru.yandex.practicum.filmorate.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.dao.FilmStorage;
+import ru.yandex.practicum.filmorate.dao.ReviewStorage;
+import ru.yandex.practicum.filmorate.dao.UserStorage;
+import ru.yandex.practicum.filmorate.exception.AlreadyExistException;
+import ru.yandex.practicum.filmorate.exception.IllegalIdException;
+import ru.yandex.practicum.filmorate.model.Review;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ReviewDbService implements ReviewService {
+    private final ReviewStorage storage;
+
+    @Override
+    public Review findReviewById(Long id) {
+        return storage.findReviewById(id);
+    }
+
+    @Override
+    public Review addReview(Review review) {
+        return storage.addReview(review);
+    }
+
+    @Override
+    public Review updateReviewById(Review review) {
+        return storage.updateReviewById(review);
+    }
+
+    @Override
+    public String deleteReviewById(Long id) {
+        return storage.deleteReviewById(id);
+    }
+
+    @Override
+    public List<Review> findAllReviews(Long filmId, Integer count) {
+        return storage.findAllReviews(filmId, count);
+    }
+
+    @Override
+    public String likeReview(Long reviewId, Long userId) {
+        return storage.likeReview(reviewId, userId);
+    }
+
+    @Override
+    public String dislikeReview(Long reviewId, Long userId) {
+        return storage.dislikeReview(reviewId, userId);
+    }
+
+    @Override
+    public String removeLikeFromReview(Long reviewId, Long userId) {
+        return storage.removeLikeFromReview(reviewId, userId);
+    }
+
+    @Override
+    public String removeDislikeFromReview(Long reviewId, Long userId) {
+        return storage.removeDislikeFromReview(reviewId, userId);
+    }
+}
