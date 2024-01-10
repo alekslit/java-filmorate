1e1edf05c8b0d6327f1aa7b1cea266230a30d380
Kirill <kirill551@yandex.ru>
2024-01-10 19:52:08 +0300

    "style"

--- src/main/java/ru/yandex/practicum/filmorate/exception/TaskException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/TaskException.java
@@ -4,6 +4,7 @@ public class TaskException extends RuntimeException {
     String s;
     String s1;
     String s2;
+
     public TaskException(String s, String s1, String s2) {
         this.s = s;
         this.s2 = s2;
