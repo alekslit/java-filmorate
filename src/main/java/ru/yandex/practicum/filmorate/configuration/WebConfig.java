0c90f80ac92b3a31052c5f29d4bc17b6210cf5c6
Soyl <Soylgr@yandex.ru>
2024-01-14 02:01:35 +0300

    fix

--- src/main/java/ru/yandex/practicum/filmorate/configuration/WebConfig.java
+++ src/main/java/ru/yandex/practicum/filmorate/configuration/WebConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import ru.yandex.practicum.filmorate.converters.SortByConverter;
-
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
